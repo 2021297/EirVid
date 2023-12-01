7981d920b3a5526ccab1b46bcc7f0dd654d1f270
miraeyu <93926128+miraeyu@users.noreply.github.com>
2023-12-01 15:51:08 +0000

    mirae first commit

--- src/eirvid/EirVid.java
+++ src/eirvid/EirVid.java
@@ -16,6 +16,7 @@ public class EirVid {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("kkk");
+        System.out.println("miare yu");
         
     }
     
